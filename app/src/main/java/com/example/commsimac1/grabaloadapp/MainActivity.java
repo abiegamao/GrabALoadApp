e346538ff1854748cd98d7b53e5473525af99c58
iCommps <joenabie@me.com>
2016-02-09 14:29:11 +0800

    font

--- app/src/main/java/com/example/commsimac1/grabaloadapp/MainActivity.java
+++ app/src/main/java/com/example/commsimac1/grabaloadapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.commsimac1.grabaloadapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Typeface caecillia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,15 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        String fontpath = "fonts/Caecillia-LightItalic.ttf";
+        toolbar.setTitle("Hi");
+
+        TextView txt= (TextView) findViewById(R.id.hello);
+        TextView ttitle= (TextView) findViewById(R.id.toolbar_title);
+        caecillia = Typeface.createFromAsset(getAssets(),fontpath);
+        txt.setTypeface(caecillia);
+        ttitle.setTypeface(caecillia);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,6 +53,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
